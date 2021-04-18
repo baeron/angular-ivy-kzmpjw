58da552be9c3b84909dc690ff8ed0dc12018b7fa
baeron <baranoffei@gmail.com>
2021-04-18 21:07:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-kzmpjw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-kzmpjw)
---
+++ src/app/login/loginView/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  template: `
+    <div>TEST</div>
+  `
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
